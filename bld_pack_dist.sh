96a7cd28aaf55f2ff82fd604622b2df566faf213
tomas <tomas.classon@gmail.com>
2021-02-15 21:08:37 +0100

    added build, pack and dist script

--- bld_pack_dist.sh
+++ bld_pack_dist.sh
@@ -1,14 +1,15 @@
 # build, pack and dist web site
+
 if [[ -z $1 ]]; then
 	echo "Missing host, abort"
 	exit 1
 else
 	TARGET=$1
 fi
-TARFILE=hfs2021.tar
+TARFILE=hfs2021.tgz
 yarn build
 yarn export
 rm -f $TARFILE
-tar xf $TARFILE out
-scp $TARFILE $TARGET
+tar czf $TARFILE out
+scp -i "/C/Program Files/PuTTY/id_rsa" $TARFILE tomas@$TARGET
 
